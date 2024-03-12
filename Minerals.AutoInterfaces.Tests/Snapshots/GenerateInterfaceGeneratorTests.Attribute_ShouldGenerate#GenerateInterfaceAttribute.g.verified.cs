0a35b37ca3cc52407fc857a2403a957755714c7d
Szymon Hałucha <99204426+SzymonHalucha@users.noreply.github.com>
2024-03-12 18:26:41 +0100

    Remove empty line in GenerateInterfaceAttribute class

--- Minerals.AutoInterfaces.Tests/Snapshots/GenerateInterfaceGeneratorTests.Attribute_ShouldGenerate#GenerateInterfaceAttribute.g.verified.cs
+++ Minerals.AutoInterfaces.Tests/Snapshots/GenerateInterfaceGeneratorTests.Attribute_ShouldGenerate#GenerateInterfaceAttribute.g.verified.cs
@@ -6,6 +6,5 @@ namespace Minerals.AutoInterfaces
     [global::System.AttributeUsage(global::System.AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class GenerateInterfaceAttribute : global::System.Attribute
     {
-
     }
 }
