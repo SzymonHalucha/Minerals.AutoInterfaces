e11d1b986715afd4a74a324b124c34b15e03fbac
Szymon Hałucha <99204426+SzymonHalucha@users.noreply.github.com>
2024-03-23 19:23:43 +0100

    Removed unused using

--- Minerals.AutoInterfaces.Benchmarks/GlobalUsings.cs
+++ Minerals.AutoInterfaces.Benchmarks/GlobalUsings.cs
@@ -4,7 +4,6 @@ global using BenchmarkDotNet.Jobs;
 global using BenchmarkDotNet.Order;
 global using BenchmarkDotNet.Configs;
 global using BenchmarkDotNet.Running;
-global using BenchmarkDotNet.Exporters;
 global using BenchmarkDotNet.Attributes;
 global using BenchmarkDotNet.Validators;
 global using BenchmarkDotNet.Diagnosers;
