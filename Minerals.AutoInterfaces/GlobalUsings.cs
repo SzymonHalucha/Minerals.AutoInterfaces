a93e329ab91144ba4278b96512c0461b4624728e
Szymon Hałucha <99204426+SzymonHalucha@users.noreply.github.com>
2024-03-24 05:18:05 +0100

    Changed code building system

--- Minerals.AutoInterfaces/GlobalUsings.cs
+++ Minerals.AutoInterfaces/GlobalUsings.cs
@@ -1,6 +1,9 @@
 global using System;
 global using System.Linq;
 global using System.Text;
+global using System.Reflection;
+global using System.Collections.Generic;
+global using System.Runtime.CompilerServices;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.Text;
 global using Microsoft.CodeAnalysis.CSharp;
